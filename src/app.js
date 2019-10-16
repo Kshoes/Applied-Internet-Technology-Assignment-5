f2f975aed72d50549ca1d6cfe24b0e2e0783a405
Kshoes <kx360@nyu.edu>
2019-10-15 22:41:29 -0400

    add authentication to database, (up to Part 2 completed)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 // app.js
+
+const express = require('express');
+const app = express();
+const db = require('./db.js');
+const mongoose = require('mongoose');
+const Review = mongoose.model('Review');
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, 'public');
+
+app.use(express.static(publicPath));
+app.use(express.urlencoded({extended: false}));
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    const reviews = Review.find({}, (err, reviews, count) => {
+        res.render('index', {reviews: reviews});
+    });
+});
+
+
+
+
+
+app.listen(3000);
+
+console.log('Server started; type CTRL+C to shut down');
